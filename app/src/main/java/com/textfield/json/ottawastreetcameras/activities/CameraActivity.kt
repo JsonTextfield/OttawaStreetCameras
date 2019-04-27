29fc41d2ff3ebb97ac2121434e48e689f1e1857a
Jason <tehblackboy@gmail.com>
2019-04-26 20:06:07 -0400

    moved save to internal storage functionality into superclass

--- app/src/main/java/com/textfield/json/ottawastreetcameras/activities/CameraActivity.kt
+++ app/src/main/java/com/textfield/json/ottawastreetcameras/activities/CameraActivity.kt
@@ -1,7 +1,5 @@
 package com.textfield.json.ottawastreetcameras.activities
 
-import android.content.Context
-import android.content.ContextWrapper
 import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
@@ -10,31 +8,22 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.ListView
 import com.android.volley.NetworkResponse
-import com.android.volley.RequestQueue
 import com.android.volley.Response
-import com.android.volley.toolbox.*
+import com.android.volley.toolbox.HttpHeaderParser
+import com.android.volley.toolbox.ImageRequest
+import com.android.volley.toolbox.StringRequest
 import com.textfield.json.ottawastreetcameras.R
+import com.textfield.json.ottawastreetcameras.StreetCamsRequestQueue
 import com.textfield.json.ottawastreetcameras.adapters.ImageAdapter
 import com.textfield.json.ottawastreetcameras.entities.Camera
 import kotlinx.android.synthetic.main.activity_camera.*
-import java.io.*
-import java.net.URL
-import java.security.KeyStore
-import java.security.cert.CertificateFactory
-import java.security.cert.X509Certificate
-import javax.net.ssl.HttpsURLConnection
-import javax.net.ssl.SSLContext
-import javax.net.ssl.TrustManagerFactory
 
 class CameraActivity : GenericActivity() {
     private var cameras = ArrayList<Camera>()
     private var timers = ArrayList<CameraRunnable>()
     private val handler = Handler()
-    private lateinit var queue: RequestQueue
-    private var sessionId = ""
     private val tag = "camera"
     private lateinit var imageAdapter: ImageAdapter
-    private var isRecording = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,83 +32,29 @@ class CameraActivity : GenericActivity() {
         cameras = intent.getParcelableArrayListExtra<Camera>("cameras")
         imageAdapter = ImageAdapter(this, cameras)
         image_listView.adapter = imageAdapter
-        createSSL()
         getSessionId()
     }
 
-    fun stop() {
-        queue.cancelAll(tag)
+    private fun stop() {
+        StreetCamsRequestQueue.getInstance(this).cancelAll(tag)
         for (timer in timers) {
             handler.removeCallbacks(timer)
         }
     }
 
     override fun onResume() {
-        createSSL()
         getSessionId()
         super.onResume()
     }
+
     override fun onPause() {
         stop()
         super.onPause()
     }
+
     override fun onDestroy() {
         stop()
         super.onDestroy()
-    }
-
-    private fun saveToInternalStorage(bitmapImage: Bitmap, id: Int) {
-        val mypath = File(ContextWrapper(applicationContext).getDir("images", Context.MODE_PRIVATE), "$id.jpg")
-
-        var fos: FileOutputStream? = null
-        try {
-            fos = FileOutputStream(mypath)
-            // Use the compress method on the BitMap object to write image to the OutputStream
-            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos)
-        } catch (e: Exception) {
-            e.printStackTrace()
-        } finally {
-            try {
-                fos?.close()
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-        }
-    }
-
-    private fun createSSL() {
-        val cf: CertificateFactory = CertificateFactory.getInstance("X.509")
-        val caInput: InputStream = BufferedInputStream(assets.open("trafficottawaca.cer"))
-        val ca: X509Certificate = caInput.use {
-            cf.generateCertificate(it) as X509Certificate
-        }
-
-        // Create a KeyStore containing our trusted CAs
-        val keyStoreType = KeyStore.getDefaultType()
-        val keyStore = KeyStore.getInstance(keyStoreType).apply {
-            load(null, null)
-            setCertificateEntry("ca", ca)
-        }
-
-        // Create a TrustManager that trusts the CAs inputStream our KeyStore
-        val tmfAlgorithm: String = TrustManagerFactory.getDefaultAlgorithm()
-        val tmf: TrustManagerFactory = TrustManagerFactory.getInstance(tmfAlgorithm).apply {
-            init(keyStore)
-        }
-
-        // Create an SSLContext that uses our TrustManager
-        val context: SSLContext = SSLContext.getInstance("TLS").apply {
-            init(null, tmf.trustManagers, null)
-        }
-
-        queue = Volley.newRequestQueue(this, object : HurlStack() {
-            override fun createConnection(url: URL): HttpsURLConnection {
-                val connection = url.openConnection() as HttpsURLConnection
-                connection.sslSocketFactory = context.socketFactory
-                connection.setRequestProperty("Cookie", sessionId)
-                return connection
-            }
-        })
     }
 
     private inner class CameraRunnable(index: Int) : Runnable {
@@ -139,7 +74,7 @@ class CameraActivity : GenericActivity() {
 
     fun download(index: Int) {
         val bmImage = image_listView.getViewByPosition(index).findViewById(R.id.source) as ImageView
-        val url = "https://traffic.ottawa.ca/map/camera?id=${imageAdapter.getItem(index)?.num}"
+        val url = "https://traffic.ottawa.ca/map/camera?id=${imageAdapter.getItem(index).num}"
 
         val request = ImageRequest(url, Response.Listener<Bitmap> { response ->
             camera_progress_bar.visibility = View.INVISIBLE
@@ -149,11 +84,11 @@ class CameraActivity : GenericActivity() {
             } catch (e: NullPointerException) {
             }
         }, 0, 0, ImageView.ScaleType.FIT_CENTER, Bitmap.Config.RGB_565, Response.ErrorListener {
-            println(it)
+            it.printStackTrace()
         })
 
         request.tag = tag
-        queue.add(request)
+        StreetCamsRequestQueue.getInstance(this).add(request)
     }
 
     private fun getSessionId() {
@@ -165,18 +100,20 @@ class CameraActivity : GenericActivity() {
                 cameraRunnable.run()
             }
         }, Response.ErrorListener {
-            println(it)
+            it.printStackTrace()
             showErrorDialogue(this)
         }) {
             override fun parseNetworkResponse(response: NetworkResponse): Response<String> {
-                sessionId = response.headers["Set-Cookie"]!!
-                return Response.success(sessionId, HttpHeaderParser.parseCacheHeaders(response))
+                val sessionId = response.headers["Set-Cookie"] ?: StreetCamsRequestQueue.sessionId
+                StreetCamsRequestQueue.sessionId = sessionId
+                return Response.success(StreetCamsRequestQueue.sessionId, HttpHeaderParser.parseCacheHeaders(response))
             }
         }
         sessionRequest.tag = tag
-        queue.add(sessionRequest)
+        StreetCamsRequestQueue.getInstance(this).add(sessionRequest)
     }
 }
+
 //https://stackoverflow.com/questions/24811536/android-listview-get-item-view-by-position/24864536
 fun ListView.getViewByPosition(pos: Int): View {
     val lastListItemPosition = firstVisiblePosition + childCount - 1
