19f5a1de9a3a7f17037dce427b93f6d95256ba55
Jason <tehblackboy@gmail.com>
2020-10-01 21:05:36 -0400

    updated SectionIndexListView.kt to be more generic for future use elsewhere.

--- app/src/main/java/com/textfield/json/ottawastreetcameras/SectionIndexListView.kt
+++ app/src/main/java/com/textfield/json/ottawastreetcameras/SectionIndexListView.kt
@@ -8,55 +8,87 @@ import android.view.MotionEvent
 import android.widget.LinearLayout
 import android.widget.ListView
 import android.widget.TextView
-import androidx.core.content.ContextCompat
-import com.textfield.json.ottawastreetcameras.activities.GenericActivity
 import kotlinx.android.synthetic.main.section_index_listview.view.*
+import java.util.*
+import kotlin.collections.HashMap
 import kotlin.math.max
+import kotlin.math.min
 
 class SectionIndexListView : LinearLayout {
-    var listview: ListView
+    lateinit var listview: ListView
         private set
-    var sectionindex: LinearLayout
+    lateinit var sectionindex: LinearLayout
         private set
+    private var indexLocation = 0
     private val index = HashMap<Char, Int>()
     private var choose = -1
+    private var indexTextViewLayout = R.layout.section_index_title
+    var selectedColour = Color.WHITE
+    var defaultColour = Color.BLUE
 
-    constructor(context: Context) : super(context)
-    constructor(context: Context, attributes: AttributeSet) : super(context, attributes)
-    constructor(context: Context, attributes: AttributeSet, int: Int) : super(context, attributes, int)
+    constructor(context: Context) : super(context) {
+        initialise()
+    }
 
-    init {
-        val mInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+    constructor(context: Context, attributes: AttributeSet) : super(context, attributes) {
+        context.theme.obtainStyledAttributes(
+                attributes,
+                R.styleable.SectionIndexListView,
+                0, 0
+        ).apply {
+            try {
+                defaultColour = getColor(R.styleable.SectionIndexListView_default_colour, Color.WHITE)
+                selectedColour = getColor(R.styleable.SectionIndexListView_selected_colour, Color.BLUE)
+                indexLocation = getInteger(R.styleable.SectionIndexListView_index_location, 0)
+            } finally {
+                recycle()
+            }
+        }
+        initialise()
+    }
+
+    constructor(context: Context, attributes: AttributeSet, int: Int) : super(context, attributes, int) {
+        initialise()
+    }
+
+    private fun initialise() {
+        val mInflater = LayoutInflater.from(context)
         mInflater.inflate(R.layout.section_index_listview, this, true)
         sectionindex = sectionIndex
         listview = listView
+        section_index_listview_layout.removeAllViews()
+        if (indexLocation == 0) {
+            section_index_listview_layout.addView(sectionindex)
+            section_index_listview_layout.addView(listview)
+        } else {
+            section_index_listview_layout.addView(listview)
+            section_index_listview_layout.addView(sectionindex)
+        }
+        section_index_listview_layout.invalidate()
         sectionIndex.setOnTouchListener { _, event ->
             val action = event.action
             val yCoordinates = event.y
             var selectedIndex = (yCoordinates / height * sectionIndex.childCount).toInt()
-            selectedIndex = max(0, selectedIndex)
+            selectedIndex = min(max(0, selectedIndex), index.size - 1)
             when (action) {
                 MotionEvent.ACTION_UP -> {
                     choose = -1
                     if (sectionIndex.getChildAt(selectedIndex) != null) {
-                        (sectionIndex.getChildAt(selectedIndex) as TextView)
-                                .setTextColor(if ((context as GenericActivity).isNightModeOn()) Color.WHITE else Color.BLACK)
+                        (sectionIndex.getChildAt(selectedIndex) as TextView).setTextColor(defaultColour)
                     }
                 }
                 else -> {
                     if (choose != selectedIndex) {
-
                         if (choose > -1) {
-                            (sectionIndex.getChildAt(choose) as TextView)
-                                    .setTextColor(if ((context as GenericActivity).isNightModeOn()) Color.WHITE else Color.BLACK)
+                            (sectionIndex.getChildAt(choose) as TextView).setTextColor(defaultColour)
                         }
                         try {
                             val t = (sectionIndex.getChildAt(selectedIndex) as TextView)
-                            t.setTextColor(ContextCompat.getColor(context, R.color.colorAccent))
+                            t.setTextColor(selectedColour)
                             listView.setSelection(index[t.text[0]]!!)
                             choose = selectedIndex
                         } catch (e: Exception) {
-
+                            e.printStackTrace()
                         }
                     }
                 }
@@ -68,17 +100,28 @@ class SectionIndexListView : LinearLayout {
     fun updateIndex() {
         index.clear()
         sectionIndex.removeAllViews()
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         for (i in 0 until listView.adapter.count) {
             //get the first character
-            val c = listView.adapter.getItem(i).toString()[0]
-            if (c !in index.keys) {
+            val t = inflater.inflate(indexTextViewLayout, this, false) as TextView
+            t.setTextColor(defaultColour)
+            val c = listView.adapter.getItem(i).toString().toUpperCase(Locale.ENGLISH)[0]
+            if (c !in index.keys && c in "ABCDEFGHIJKLMNOPQRESTUVWXYZ") {
                 index[c] = i
-                val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-                val t = inflater.inflate(R.layout.section_index_title, this, false) as TextView
-                t.setTextColor(if ((context as GenericActivity).isNightModeOn()) Color.WHITE else Color.BLACK)
                 t.text = c.toString()
                 sectionIndex.addView(t)
                 sectionindex.invalidate()
+            } else if (c in "0123456789" && !index.containsKey('#')) {
+                index['#'] = 0
+                t.text = "#"
+                sectionIndex.addView(t)
+                sectionindex.invalidate()
+            } else if (c !in "0123456789ABCDEFGHIJKLMNOPQRESTUVWXYZ") {
+                index['*'] = i
+                t.text = "*"
+                sectionIndex.addView(t)
+                sectionindex.invalidate()
+                break
             }
         }
     }
