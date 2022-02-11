8714d80e2bdadc3204deb3daf233eb20893526af
Jason <tehblackboy@gmail.com>
2022-02-11 18:38:32 -0500

    updating tests

--- app/src/androidTest/java/com/textfield/json/ottawastreetcameras/MainActivityTest.kt
+++ app/src/androidTest/java/com/textfield/json/ottawastreetcameras/MainActivityTest.kt
@@ -1,15 +1,26 @@
 package com.textfield.json.ottawastreetcameras
 
-import androidx.test.rule.ActivityTestRule
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.filters.LargeTest
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import com.textfield.json.ottawastreetcameras.activities.MainActivity
-import org.junit.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4ClassRunner::class)
+@LargeTest
 class MainActivityTest {
-    @Rule
-    var mActivityRule = ActivityTestRule(MainActivity::class.java)
+    @get:Rule
+    val mActivityRule: ActivityScenarioRule<MainActivity> = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
-    fun test1() {
+    fun listGoesOverTheFold() {
+        onView(withText("StreetCams")).check(matches(isDisplayed()))
+        onView(withId(R.id.section_index_listview)).check(matches(isDisplayed()))
     }
 }
 
