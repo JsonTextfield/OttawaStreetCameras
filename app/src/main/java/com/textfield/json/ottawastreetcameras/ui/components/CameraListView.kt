3246af624caa33336835f1a73742a98d73c6f842
jason <tehblackboy@gmail.com>
2023-07-13 20:30:21 -0400

    partially implemented swipe to dismiss

--- app/src/main/java/com/textfield/json/ottawastreetcameras/ui/components/CameraListView.kt
+++ app/src/main/java/com/textfield/json/ottawastreetcameras/ui/components/CameraListView.kt
@@ -1,17 +1,31 @@
 package com.textfield.json.ottawastreetcameras.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.VisibilityOff
+import androidx.compose.material3.DismissDirection
+import androidx.compose.material3.DismissState
+import androidx.compose.material3.DismissValue
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SwipeToDismiss
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.pluralStringResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.textfield.json.ottawastreetcameras.CameraManager
 import com.textfield.json.ottawastreetcameras.R
 import com.textfield.json.ottawastreetcameras.entities.Camera
 
@@ -25,10 +39,11 @@ fun CameraListView(
 ) {
     LazyColumn(modifier = modifier, state = listState) {
         items(cameras, { camera -> camera.num }) { camera ->
-            /*val cameraManager = CameraManager.getInstance()
-            val dismissState = rememberDismissState()
+            val cameraManager = CameraManager.getInstance()
+            val dismissState = remember { DismissState(DismissValue.Default) }
             val context = LocalContext.current
             if (dismissState.isDismissed(DismissDirection.EndToStart) || dismissState.isDismissed(DismissDirection.StartToEnd)) {
+                camera.isVisible = !camera.isVisible
                 cameraManager.hideCamera(context, camera)
             }
             SwipeToDismiss(
@@ -41,7 +56,13 @@ fun CameraListView(
                             .weight(0.1f)
                             .padding(10.dp)
                     )
-                    Spacer(modifier = Modifier.weight(0.5f))
+                    Text(
+                        stringResource(if (camera.isVisible) R.string.hide else R.string.unhide),
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .align(Alignment.CenterVertically)
+                            .weight(0.5f)
+                    )
                     Icon(
                         Icons.Rounded.VisibilityOff, "", modifier = Modifier
                             .align(Alignment.CenterVertically)
@@ -50,8 +71,8 @@ fun CameraListView(
                     )
                 },
                 dismissContent = {
-                })*/
-            CameraListTile(camera, onItemClick)
+                    CameraListTile(camera, onItemClick)
+                })
         }
         item {
             Text(
