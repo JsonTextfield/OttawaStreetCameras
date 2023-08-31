553bae707d05bf3ccf5d5d2ada27724d428c5f27
jason <tehblackboy@gmail.com>
2023-08-31 09:42:26 -0400

    migrated to gradle.kts files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-
 buildscript {
     repositories {
         mavenCentral()
         google()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:8.1.0'
-        classpath 'org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20'
-        classpath 'com.google.android.gms:oss-licenses-plugin:0.10.6'
+        classpath("com.android.tools.build:gradle:8.1.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
+        classpath("com.google.android.gms:oss-licenses-plugin:0.10.6")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -17,14 +16,10 @@ buildscript {
 
 allprojects {
     repositories {
-
-        maven {
-            url 'https://maven.google.com/'
-            name 'Google'
-        }
+        //maven ("https://maven.google.com/")
     }
 }
 
-tasks.register('clean', Delete) {
-    delete rootProject.buildDir
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
