6fe79992b677fbbc645e8200460d257e6f5b6cbd
jason <tehblackboy@gmail.com>
2023-08-31 14:10:23 -0400

    decoupling SortModeMenu and ViewModeMenu and making them more generic

--- app/src/main/java/com/textfield/json/ottawastreetcameras/ui/components/menu/SortModeMenu.kt
+++ app/src/main/java/com/textfield/json/ottawastreetcameras/ui/components/menu/SortModeMenu.kt
@@ -2,37 +2,27 @@ package com.textfield.json.ottawastreetcameras.ui.components.menu
 
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import com.textfield.json.ottawastreetcameras.CameraViewModel
 import com.textfield.json.ottawastreetcameras.R
 import com.textfield.json.ottawastreetcameras.SortMode
 
 @Composable
-fun SortModeMenu(cameraViewModel: CameraViewModel, expanded: Boolean, onItemSelected: () -> Unit) {
-    val cameraState by cameraViewModel.cameraState.collectAsState()
-    val context = LocalContext.current
-    DropdownMenu(expanded = expanded, onDismissRequest = { onItemSelected() }) {
-        fun setSortMode(sortMode: SortMode) {
-            cameraViewModel.changeSortMode(context, sortMode)
-            onItemSelected()
-        }
+fun SortModeMenu(expanded: Boolean, currentSortMode: SortMode, onItemSelected: (sortMode: SortMode) -> Unit) {
+    DropdownMenu(expanded = expanded, onDismissRequest = { onItemSelected(currentSortMode) }) {
         RadioMenuItem(
             title = stringResource(R.string.sort_by_name),
-            selected = cameraState.sortMode == SortMode.NAME,
-            onClick = { setSortMode(SortMode.NAME) },
+            selected = currentSortMode == SortMode.NAME,
+            onClick = { onItemSelected(SortMode.NAME) },
         )
         RadioMenuItem(
             title = stringResource(R.string.sort_by_distance),
-            selected = cameraState.sortMode == SortMode.DISTANCE,
-            onClick = { setSortMode(SortMode.DISTANCE) },
+            selected = currentSortMode == SortMode.DISTANCE,
+            onClick = { onItemSelected(SortMode.DISTANCE) },
         )
         RadioMenuItem(
             title = stringResource(R.string.sort_by_neighbourhood),
-            selected = cameraState.sortMode == SortMode.NEIGHBOURHOOD,
-            onClick = { setSortMode(SortMode.NEIGHBOURHOOD) },
+            selected = currentSortMode == SortMode.NEIGHBOURHOOD,
+            onClick = { onItemSelected(SortMode.NEIGHBOURHOOD) },
         )
     }
 }
--- app/src/main/java/com/textfield/json/ottawastreetcameras/ui/components/menu/ViewModeMenu.kt
+++ app/src/main/java/com/textfield/json/ottawastreetcameras/ui/components/menu/ViewModeMenu.kt
@@ -2,37 +2,27 @@ package com.textfield.json.ottawastreetcameras.ui.components.menu
 
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import com.textfield.json.ottawastreetcameras.CameraViewModel
 import com.textfield.json.ottawastreetcameras.R
 import com.textfield.json.ottawastreetcameras.ViewMode
 
 @Composable
-fun ViewModeMenu(cameraViewModel: CameraViewModel, expanded: Boolean, onItemSelected: () -> Unit) {
-    val cameraState by cameraViewModel.cameraState.collectAsState()
-    val context = LocalContext.current
-    DropdownMenu(expanded = expanded, onDismissRequest = { onItemSelected() }) {
-        fun setViewMode(viewMode: ViewMode) {
-            cameraViewModel.changeViewMode(context, viewMode)
-            onItemSelected()
-        }
+fun ViewModeMenu(expanded: Boolean, currentViewMode: ViewMode, onItemSelected: (viewMode: ViewMode) -> Unit) {
+    DropdownMenu(expanded = expanded, onDismissRequest = { onItemSelected(currentViewMode) }) {
         RadioMenuItem(
             title = stringResource(R.string.list),
-            selected = cameraState.viewMode == ViewMode.LIST,
-            onClick = { setViewMode(ViewMode.LIST) },
+            selected = currentViewMode == ViewMode.LIST,
+            onClick = { onItemSelected(ViewMode.LIST) },
         )
         RadioMenuItem(
             title = stringResource(R.string.map),
-            selected = cameraState.viewMode == ViewMode.MAP,
-            onClick = { setViewMode(ViewMode.MAP) },
+            selected = currentViewMode == ViewMode.MAP,
+            onClick = { onItemSelected(ViewMode.MAP) },
         )
         RadioMenuItem(
             title = stringResource(R.string.gallery),
-            selected = cameraState.viewMode == ViewMode.GALLERY,
-            onClick = { setViewMode(ViewMode.GALLERY) },
+            selected = currentViewMode == ViewMode.GALLERY,
+            onClick = { onItemSelected(ViewMode.GALLERY) },
         )
     }
 }
