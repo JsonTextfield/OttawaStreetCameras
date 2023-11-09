4e8ecfd2dfa6e2219b1fc0f0f81723030cb11aff
jason <tehblackboy@gmail.com>
2023-11-09 10:48:01 -0500

    updated radio button menus

--- app/src/main/java/com/textfield/json/ottawastreetcameras/ui/components/menu/SortModeMenu.kt
+++ app/src/main/java/com/textfield/json/ottawastreetcameras/ui/components/menu/SortModeMenu.kt
@@ -3,26 +3,17 @@ package com.textfield.json.ottawastreetcameras.ui.components.menu
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
-import com.textfield.json.ottawastreetcameras.R
 import com.textfield.json.ottawastreetcameras.SortMode
 
 @Composable
 fun SortModeMenu(expanded: Boolean, currentSortMode: SortMode, onItemSelected: (sortMode: SortMode) -> Unit) {
     DropdownMenu(expanded = expanded, onDismissRequest = { onItemSelected(currentSortMode) }) {
-        RadioMenuItem(
-            title = stringResource(R.string.sort_by_name),
-            selected = currentSortMode == SortMode.NAME,
-            onClick = { onItemSelected(SortMode.NAME) },
-        )
-        RadioMenuItem(
-            title = stringResource(R.string.sort_by_distance),
-            selected = currentSortMode == SortMode.DISTANCE,
-            onClick = { onItemSelected(SortMode.DISTANCE) },
-        )
-        RadioMenuItem(
-            title = stringResource(R.string.sort_by_neighbourhood),
-            selected = currentSortMode == SortMode.NEIGHBOURHOOD,
-            onClick = { onItemSelected(SortMode.NEIGHBOURHOOD) },
-        )
+        SortMode.values().forEach {
+            RadioMenuItem(
+                title = stringResource(it.key),
+                selected = currentSortMode == it,
+                onClick = { onItemSelected(it) },
+            )
+        }
     }
 }
--- app/src/main/java/com/textfield/json/ottawastreetcameras/ui/components/menu/ViewModeMenu.kt
+++ app/src/main/java/com/textfield/json/ottawastreetcameras/ui/components/menu/ViewModeMenu.kt
@@ -3,26 +3,17 @@ package com.textfield.json.ottawastreetcameras.ui.components.menu
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
-import com.textfield.json.ottawastreetcameras.R
 import com.textfield.json.ottawastreetcameras.ViewMode
 
 @Composable
 fun ViewModeMenu(expanded: Boolean, currentViewMode: ViewMode, onItemSelected: (viewMode: ViewMode) -> Unit) {
     DropdownMenu(expanded = expanded, onDismissRequest = { onItemSelected(currentViewMode) }) {
-        RadioMenuItem(
-            title = stringResource(R.string.list),
-            selected = currentViewMode == ViewMode.LIST,
-            onClick = { onItemSelected(ViewMode.LIST) },
-        )
-        RadioMenuItem(
-            title = stringResource(R.string.map),
-            selected = currentViewMode == ViewMode.MAP,
-            onClick = { onItemSelected(ViewMode.MAP) },
-        )
-        RadioMenuItem(
-            title = stringResource(R.string.gallery),
-            selected = currentViewMode == ViewMode.GALLERY,
-            onClick = { onItemSelected(ViewMode.GALLERY) },
-        )
+        ViewMode.values().forEach {
+            RadioMenuItem(
+                title = stringResource(it.key),
+                selected = currentViewMode == it,
+                onClick = { onItemSelected(it) },
+            )
+        }
     }
 }
