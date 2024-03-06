e414f4fbe929f6c2191fb6a7ea8b04785cb0e990
jason <tehblackboy@gmail.com>
2024-03-05 23:57:50 -0500

    updated tests

--- app/src/androidTest/java/com/textfield/json/ottawastreetcameras/MainActivityTest.kt
+++ app/src/androidTest/java/com/textfield/json/ottawastreetcameras/MainActivityTest.kt
@@ -1,16 +1,10 @@
 package com.textfield.json.ottawastreetcameras
 
-import androidx.test.espresso.Espresso.*
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.filters.LargeTest
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-import androidx.test.platform.app.InstrumentationRegistry
 import com.textfield.json.ottawastreetcameras.ui.activities.MainActivity
 import org.junit.Rule
-import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4ClassRunner::class)
@@ -18,26 +12,5 @@ import org.junit.runner.RunWith
 class MainActivityTest {
     @get:Rule
     val mActivityRule: ActivityScenarioRule<MainActivity> = ActivityScenarioRule(MainActivity::class.java)
-
-    @Test
-    fun listGoesOverTheFold() {
-        onView(withText("StreetCams")).check(matches(isDisplayed()))
-    }
-
-    @Test
-    fun listViewLoads() {
-        onData(withId(R.id.section_index_listview)).atPosition(0).check(matches(isDisplayed()))
-    }
-
-    @Test
-    fun sectionIndexVisibleDefault() {
-        onView(withId(R.id.sectionIndex)).check(matches(isDisplayed()))
-    }
-
-    @Test
-    fun sectionIndexInvisibleAfterSortByDistance() {
-        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getInstrumentation().context)
-        onView(withText(R.string.sort_by_distance)).perform(click())
-    }
 }
 
