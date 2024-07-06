821b13b247f00ab6f118a6e3254da1df6f96d5ef
jason <tehblackboy@gmail.com>
2024-07-05 22:29:09 -0400

    added interfaces and implementations for repository and data source

---
+++ app/src/main/java/com/textfield/json/ottawastreetcameras/data/SupabaseCameraDataSource.kt
@@ -0,0 +1,36 @@
+package com.textfield.json.ottawastreetcameras.data
+
+import android.content.res.Resources
+import com.textfield.json.ottawastreetcameras.R
+import com.textfield.json.ottawastreetcameras.entities.Camera
+import com.textfield.json.ottawastreetcameras.entities.CameraApiModel
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.postgrest.from
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class SupabaseCameraDataSource : CameraDataSource {
+    private val supabase by lazy {
+        createSupabaseClient(
+            supabaseUrl = "https://nacudfxzbqaesoyjfluh.supabase.co",
+            supabaseKey = Resources.getSystem().getString(R.string.supabase_key)
+        ) {
+            install(Postgrest)
+        }
+    }
+
+    override suspend fun getAllCameras(): Flow<List<Camera>> {
+        return flow {
+            emit(
+                supabase.from("cameras").select {
+                    filter {
+                        eq("city", "ottawa")
+                    }
+                }.decodeList<CameraApiModel>().map {
+                    it.toCamera()
+                }
+            )
+        }
+    }
+}
