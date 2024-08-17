620117d6c518547198549755baef08b02cd6feff
jason <tehblackboy@gmail.com>
2024-08-17 10:08:20 -0400

    deleted CameraActivity.kt and implemented compose navigation in MainActivity

--- app/src/main/java/com/textfield/json/ottawastreetcameras/ui/main/MainActivity.kt
+++ app/src/main/java/com/textfield/json/ottawastreetcameras/ui/main/MainActivity.kt
@@ -3,11 +3,40 @@ package com.textfield.json.ottawastreetcameras.ui.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.textfield.json.ottawastreetcameras.ui.camera.CameraScreen
 import com.textfield.json.ottawastreetcameras.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { AppTheme { MainScreen() } }
+        setContent {
+            AppTheme {
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "main") {
+                    composable("main") {
+                        MainScreen { selectedCameras, isShuffling ->
+                            navController.navigate("cameras/${selectedCameras.joinToString(",") { it.id }}/$isShuffling")
+                        }
+                    }
+                    composable(
+                        "cameras/{ids}/{isShuffling}",
+                        arguments = listOf(
+                            navArgument("ids") { type = NavType.StringType },
+                            navArgument("isShuffling") { type = NavType.BoolType },
+                        )
+                    ) {
+                        CameraScreen(
+                            ids = it.arguments?.getString("ids") ?: "",
+                            isShuffling = it.arguments?.getBoolean("isShuffling") ?: false,
+                        )
+                    }
+                }
+            }
+        }
     }
 }
