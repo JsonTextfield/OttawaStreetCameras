ca5f99b44f0eb76158dea8467368339455ac00c4
jason <tehblackboy@gmail.com>
2024-10-11 00:20:10 -0400

    added a provider for SharedPreferences

--- app/src/main/java/com/textfield/json/ottawastreetcameras/di/AppModule.kt
+++ app/src/main/java/com/textfield/json/ottawastreetcameras/di/AppModule.kt
@@ -1,20 +1,24 @@
 package com.textfield.json.ottawastreetcameras.di
 
+import android.content.Context
+import android.content.SharedPreferences
 import com.textfield.json.ottawastreetcameras.R
 import com.textfield.json.ottawastreetcameras.StreetCamsApp
 import com.textfield.json.ottawastreetcameras.data.CameraRepository
 import com.textfield.json.ottawastreetcameras.data.ICameraRepository
-import com.textfield.json.ottawastreetcameras.data.SupabaseCameraDataSource
+import com.textfield.json.ottawastreetcameras.data.LocalCameraDataSource
 import com.textfield.json.ottawastreetcameras.ui.main.MainViewModel
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.postgrest.Postgrest
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -33,12 +37,18 @@ object AppModule {
     val dispatcher: CoroutineDispatcher = Dispatchers.IO
 
     @Provides
-    fun provideCameraRepository(): ICameraRepository {
-        return CameraRepository(SupabaseCameraDataSource(supabase))
+    fun provideSharedPreferences(@ApplicationContext context: Context): SharedPreferences {
+        return context.getSharedPreferences(context.packageName, Context.MODE_PRIVATE)
+    }
+
+    @Singleton
+    @Provides
+    fun provideCameraRepository(@ApplicationContext context: Context): ICameraRepository {
+        return CameraRepository(LocalCameraDataSource(context))
     }
 
     @Provides
-    fun provideMainViewModel(): MainViewModel {
-        return MainViewModel(provideCameraRepository())
+    fun provideMainViewModel(@ApplicationContext context: Context): MainViewModel {
+        return MainViewModel(provideCameraRepository(context))
     }
 }
