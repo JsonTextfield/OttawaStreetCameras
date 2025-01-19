9ef0d91b7b44a0d5f808d87c1a985a159db3f86d
jason <tehblackboy@gmail.com>
2025-01-18 19:19:27 -0500

    fixed swipe to dismiss

--- app/src/main/java/com/textfield/json/ottawastreetcameras/ui/main/MainContent.kt
+++ app/src/main/java/com/textfield/json/ottawastreetcameras/ui/main/MainContent.kt
@@ -6,13 +6,15 @@ import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.SnackbarResult
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.textfield.json.ottawastreetcameras.R
 import com.textfield.json.ottawastreetcameras.entities.Camera
-import kotlinx.coroutines.launch
 
 @Composable
 fun MainContent(
@@ -32,23 +34,24 @@ fun MainContent(
         }
     }
     val onItemLongClick = { camera: Camera -> mainViewModel.selectCamera(camera) }
-    val scope = rememberCoroutineScope()
-    val onItemDismissed: (Camera) -> Unit = { camera ->
-        mainViewModel.hideCameras(listOf(camera))
-        val visibilityStringId = if (camera.isVisible) R.string.unhidden else R.string.hidden
-        scope.launch {
+    var dismissedCamera by remember { mutableStateOf<Camera?>(null) }
+    LaunchedEffect(dismissedCamera) {
+        dismissedCamera?.let {
+            mainViewModel.hideCameras(listOf(it))
+            val visibilityStringId = if (it.isVisible) R.string.hidden else R.string.unhidden
             val result = snackbarHostState.showSnackbar(
                 message = context.getString(
                     R.string.camera_visibility_changed,
-                    camera.name,
+                    it.name,
                     context.getString(visibilityStringId)
                 ),
                 actionLabel = context.getString(R.string.undo),
                 duration = SnackbarDuration.Short,
             )
             if (result == SnackbarResult.ActionPerformed) {
-                mainViewModel.hideCameras(listOf(camera))
+                mainViewModel.hideCameras(listOf(it))
             }
+            dismissedCamera = null
         }
     }
     when (cameraState.viewMode) {
@@ -58,7 +61,7 @@ fun MainContent(
                 listState = listState,
                 onItemClick = onItemClick,
                 onItemLongClick = onItemLongClick,
-                onItemDismissed = onItemDismissed,
+                onItemDismissed = { dismissedCamera = it },
                 onFavouriteClick = { mainViewModel.favouriteCameras(listOf(it)) },
             )
         }
