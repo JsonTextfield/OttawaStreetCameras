bec96c9d5d7c1f6729177bf7a551c65811a2d3b8
jason <tehblackboy@gmail.com>
2025-03-01 13:54:52 -0500

    improved loading time of cameras

--- app/src/main/java/com/textfield/json/ottawastreetcameras/data/CameraRepository.kt
+++ app/src/main/java/com/textfield/json/ottawastreetcameras/data/CameraRepository.kt
@@ -3,7 +3,11 @@ package com.textfield.json.ottawastreetcameras.data
 import com.textfield.json.ottawastreetcameras.entities.Camera
 
 class CameraRepository(private val dataSource: ICameraDataSource) : ICameraRepository {
+    private var allCameras = emptyList<Camera>()
     override suspend fun getAllCameras(): List<Camera> {
-        return dataSource.getAllCameras()
+        if (allCameras.isEmpty()) {
+            allCameras = dataSource.getAllCameras()
+        }
+        return allCameras
     }
 }
