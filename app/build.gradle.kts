468ef627bfc127400834f7ecc07bb7d78e04c0b6
jason <tehblackboy@gmail.com>
2025-04-01 21:52:39 -0400

    version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         minSdk = 28
         compileSdk = 35
         targetSdk = 35
-        versionCode = 36
-        versionName = "2.3.3"
+        versionCode = 37
+        versionName = "2.4.0"
         vectorDrawables { useSupportLibrary = true }
     }
     kotlinOptions {
