f00af8a91924761b6b62171049e7db712e746d41
jasonbromfield <jasonhowlook@gmail.com>
2025-09-12 15:50:24 -0400

    ui updates

---
+++ app/src/main/java/com/textfield/json/ottawastreetcameras/entities/City.kt
@@ -0,0 +1,9 @@
+package com.textfield.json.ottawastreetcameras.entities
+
+enum class City(val cityName: String) {
+    OTTAWA("ottawa"),
+    TORONTO("toronto"),
+    VANCOUVER("vancouver"),
+    CALGARY("calgary"),
+    SURREY("surrey"),
+}
