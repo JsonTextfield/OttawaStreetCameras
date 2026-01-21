503d07b00d2d1c97b64f488516fbe71fb8151de6
jasonbromfield <jasonhowlook@gmail.com>
2026-01-20 21:53:33 -0500

    update project name and refine Google repository configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,33 @@
+rootProject.name = "StreetCams"
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
 pluginManagement {
     repositories {
-        google()
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
         mavenCentral()
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        google()
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
         mavenCentral()
     }
 }
 
-rootProject.name = "Ottawa StreetCams"
 include(":app")
 include(":core")
